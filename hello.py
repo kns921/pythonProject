a3594b580265f6485901d74900ccf36c093e3b9a
kns921 <86966139+kns921@users.noreply.github.com>
2024-07-17 10:44:07 +0500

    Initial commit

---
+++ hello.py
@@ -0,0 +1,3 @@
+def some():
+    print("hello")
+# some()
---
+++ start.py
@@ -0,0 +1,9 @@
+import hello
+hello.some()
+
+import math
+print(math.pi)
+
+import random
+r = random.randrange(0, 10000)
+print('user' + str(r))
