d2e1e99c5c0c19bb998dedeb802d19823355cf22
kns921 <86966139+kns921@users.noreply.github.com>
2024-08-01 09:49:31 +0500

    Initial commit

---
+++ base_person.py
@@ -0,0 +1,15 @@
+class Person():
+    def __init__(self, name, age, height, weight):
+        self.name = name
+        self.age = age
+        self.height = height
+        self.weight = weight
+        print('Персона создана')
+
+    def description_prsson(self):
+        description = self.name + ', ему ' + str(self.age) + ', его рост ' + str(self.height) + ', его вес ' + str(
+            self.weight)
+        print('Новый человек создан: ' + description)
+
+human = Person('Nikolay', 31, 172, 90)
+human.description_prsson()
--- person_1.py
+++ person_1.py
@@ -7,14 +7,18 @@ class Person():
         self.height = height
         self.weight = weight
         # print('Персона создана')
+
     def description(self):
         description = self.name + ', ему ' + str(self.age) + ', его рост ' + str(self.height) + ', его вес ' + str(
             self.weight)
         print('Новый человек создан: ' + description)
+
     def sing(self):
         print(self.name + ' поет')
+
     def dance(self):
         print(self.name + ' танцует')
+
 
 # man = Person('Nik', 31, 172, 80)
 # woman = Person('Anastasia', 28, 156, 50)
@@ -28,9 +32,11 @@ class Warrior(Person):
         super().__init__(name, age, height, weight)
         self.rage = 100
         # инициализировали атрибуты класса
+
     def get_rage(self):
         print('Заряд ярости равен: ' + str(self.rage))
-    def description(self):
+
+    def description_warrior(self):
         description = self.name + ', его вес ' + str(self.weight) + ', его заряд ярости ' + str(self.rage)
         # print('Новый воин создан: ' + description)
         return description
@@ -39,7 +45,7 @@ class Warrior(Person):
 warrior = Warrior('Konan', 31, 210, 130)
 # warrior.description()
 # warrior.get_rage()
-print('Новый воин создан: ' + warrior.description())
+print('Новый воин создан: ' + warrior.description_warrior())
 # man = Person('Anakoliy', 31, 172, 90)
 # man.description()
 #
