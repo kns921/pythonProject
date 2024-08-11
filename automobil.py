8ad96b67f2891beaf1b04002ff38c84b02b1cf2e
kns921 <86966139+kns921@users.noreply.github.com>
2024-08-11 13:29:43 +0500

    Initial commit

---
+++ automobil.py
@@ -0,0 +1,14 @@
+class Car():
+    def __init__(self, model, year, volume_engine, price, mileage, wheels):
+        self.model = 'rocket'
+        self.year = 2024
+        self.volume_engine = 2000
+        self.price = 2000000
+        self.mileage = 0
+        self.wheels = 4
+
+    # def car_description(self):
+    #     description = 'модель ' + self.model, 'год выпуска' + str(self.year), 'объем двигателя ' + str(self.volume_engine), 'price is ' + str(self.price), 'пробег ' + str(self.mileage), 'кол-во колес ' + str(self.wheels)
+    #     print('Новое авто: ' + description)
+
+# car = Car()
