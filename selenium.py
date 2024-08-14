d491d358e76aa3eaaec6a43acb25cf354ad6bb6f
kns921 <86966139+kns921@users.noreply.github.com>
2024-08-14 20:36:14 +0500

    Initial commit

--- selenium.py
+++
@@ -1,4 +0,0 @@
-from selenium import webriver
-
-driver = webdriver.Chrome(executable_path = 'selenium-3.141.0/selenium/webdriver/chrome/chromedriver-mac-arm64/chromedriver')
-driver.get('https://www.saucedemo.com')
