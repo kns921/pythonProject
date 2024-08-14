14a40ba15019df26429af517c06b877fea41e628
kns921 <86966139+kns921@users.noreply.github.com>
2024-08-14 22:53:29 +0500

    Initial commit

--- test_open_chrome.py
+++ test_open_chrome.py
@@ -1,5 +1,6 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
@@ -8,3 +9,12 @@ driver = webdriver.Chrome(options=options, service=g)
 base_url = 'https://www.saucedemo.com/'
 driver.get(base_url)
 driver.maximize_window()
+
+user_name = driver.find_element(By.XPATH, '//*[@id="user-name"]')
+user_name.send_keys('standard_user')
+
+user_password = driver.find_element(By.XPATH, '//*[@id="password"]')
+user_password.send_keys('secret_sauce')
+
+button_login = driver.find_element(By.XPATH, '//*[@id="login-button"]')
+button_login.click()
