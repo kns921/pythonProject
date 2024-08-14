506320620307a4345df3bd8861d296b574e8b337
kns921 <86966139+kns921@users.noreply.github.com>
2024-08-14 20:56:38 +0500

    Initial commit

---
+++ test_open_chrome.py
@@ -0,0 +1,10 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+g = Service()
+driver = webdriver.Chrome(options=options, service=g)
+base_url = 'https://www.saucedemo.com/'
+driver.get(base_url)
+driver.maximize_window()
