10d63aea43d76488d54bd43b42b7d953f75b1941
kns921 <86966139+kns921@users.noreply.github.com>
2024-08-15 10:38:51 +0500

    Initial commit

---
+++ test_1.py
@@ -0,0 +1,41 @@
+from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
+from selenium.webdriver.common.by import By
+
+# driver = webdriver.Chrome(executable_path='selenium-3.141.0/selenium/webdriver/chrome')
+options = webdriver.ChromeOptions()
+options.add_experimental_option("detach", True)
+g = Service()
+driver = webdriver.Chrome(options=options, service=g)
+bace_url = 'https://www.saucedemo.com/'
+driver.get(bace_url)
+driver.maximize_window()
+
+login_window = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[1]')
+value_login_window = login_window.text
+print(value_login_window)
+assert value_login_window == 'Swag Labs'
+print('OK')
+
+user_name = driver.find_element(By.XPATH, '//*[@id="user-name"]')
+user_name.send_keys('standard_user')
+
+user_password = driver.find_element(By.XPATH, '//*[@id="password"]')
+user_password.send_keys('secret_sauce')
+
+button_login = driver.find_element(By.XPATH, '//*[@id="login-button"]')
+button_login.click()
+print('Click login button')
+
+url_home = 'https://www.saucedemo.com/inventory.html'
+get_url = driver.current_url
+print(get_url)
+assert url_home == get_url
+print('OK')
+
+text_product = driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/span')
+value_text_product = text_product.text
+print(value_text_product)
+assert value_text_product == 'Products'
+print('OK')
+
