59bc6f32ef5c35fc7de5e0a65c09e4c626ed3483
kns921 <86966139+kns921@users.noreply.github.com>
2024-08-23 01:32:41 +0500

    Initial commit

--- test_8_smoke.py
+++ test_8_smoke.py
@@ -5,8 +5,8 @@ from selenium.webdriver.common.by import By
 
 options = webdriver.ChromeOptions()
 options.add_experimental_option("detach", True)
-options.add_argument('--headless')
 # запуск тестов без открытия браузера
+options.add_argument('--headless')
 g = Service()
 driver = webdriver.Chrome(options=options, service=g)
 bace_url = 'https://www.saucedemo.com/'
@@ -18,7 +18,7 @@ login_window = driver.find_element(By.XPATH, '//*[@id="root"]/div/div[1]')
 value_login_window = login_window.text
 print(value_login_window)
 assert value_login_window == 'Swag Labs'
-print('OK')
+print('Autorization form OK')
 
 user_name = driver.find_element(By.XPATH, '//*[@id="user-name"]')
 user_name.send_keys('standard_user')
@@ -34,17 +34,17 @@ url_home = 'https://www.saucedemo.com/inventory.html'
 get_url = driver.current_url
 print(get_url)
 assert url_home == get_url
-print('OK')
+print('URL OK')
 
 text_product = driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/span')
 value_text_product = text_product.text
 print(value_text_product)
 assert value_text_product == 'Products'
-print('OK')
+print('Autorization test OK')
 
 # Screen Autorization
 now_date = datetime.datetime.now().strftime("%H.%M.%S-%Y.%m.%d")
-name_screenshot = 'Autorization OK Screenshot ' + now_date + '.png'
+name_screenshot = 'Autorization Screenshot ' + now_date + '.png'
 driver.save_screenshot(f'screenshots/{name_screenshot}')
 print(name_screenshot)
 
@@ -80,11 +80,11 @@ print('Click Basket')
 
 basket_page = driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/span')
 value_basket_page = basket_page.text
-print(value_basket_page)
+print('Test basket OK  name page: ' + value_basket_page)
 
 # Screen Basket
 now_date = datetime.datetime.now().strftime("%H.%M.%S-%Y.%m.%d")
-name_screenshot = 'Basket ' + now_date + '.png'
+name_screenshot = 'Basket Screenshot' + now_date + '.png'
 driver.save_screenshot(f'screenshots/{name_screenshot}')
 print(name_screenshot)
 
@@ -100,7 +100,7 @@ print(value_checkout_page)
 
 # Screen Checkout page
 now_date = datetime.datetime.now().strftime("%H.%M.%S-%Y.%m.%d")
-name_screenshot = 'Checkout page ' + now_date + '.png'
+name_screenshot = 'Checkout page Screenshot' + now_date + '.png'
 driver.save_screenshot(f'screenshots/{name_screenshot}')
 print(name_screenshot)
 
@@ -121,3 +121,83 @@ post_code_inp.send_keys(str(post_code))
 continue_button = driver.find_element(By.XPATH, '//*[@id="continue"]')
 continue_button.click()
 print('Checkout Continue')
+
+"""Checkout: Overview"""
+# Overview page
+overview_page = driver.find_element(By.XPATH, '//*[@id="header_container"]/div[2]/span')
+value_overview_page = overview_page.text
+print(value_overview_page)
+
+# Screen Overview
+now_date = datetime.datetime.now().strftime("%H.%M.%S-%Y.%m.%d")
+name_screenshot = 'Overview Screenshot' + now_date + '.png'
+driver.save_screenshot(f'screenshots/{name_screenshot}')
+print(name_screenshot)
+
+# Checkout goods
+goods_1 = driver.find_element(By.XPATH, '//*[@id="item_4_title_link"]/div')
+value_goods_1 = goods_1.text
+print('goods 1 name: ' + value_goods_1)
+
+goods_1_price = driver.find_element(By.XPATH, '//*[@id="checkout_summary_container"]/div/div[1]/div[3]/div[2]/div['
+                                              '2]/div')
+value_goods_1_price = goods_1_price.text
+print('price 1: ' + value_goods_1_price)
+
+goods_2 = driver.find_element(By.XPATH, '//*[@id="item_0_title_link"]/div')
+value_goods_2 = goods_2.text
+print('goods 2 name: ' + value_goods_2)
+
+goods_2_price = driver.find_element(By.XPATH, '//*[@id="checkout_summary_container"]/div/div[1]/div[4]/div[2]/div['
+                                              '2]/div')
+value_goods_2_price = goods_2_price.text
+print('price 2: ' + value_goods_2_price)
+
+# Assert price
+assert value_goods_1_price == value_price_goods_1
+assert value_goods_2_price == value_price_goods_2
+print('Assert price OK')
+
+# Test sum goods
+num_price_1 = value_goods_1_price
+num_price_1 = num_price_1[1:]
+
+num_price_2 = value_goods_2_price
+num_price_2 = num_price_2[1:]
+
+# Test total price & Tax
+test_total_price = float(num_price_1) + float(num_price_2)
+
+item_total = driver.find_element(By.XPATH, '//*[@id="checkout_summary_container"]/div/div[2]/div[6]')
+value_item_total = item_total.text
+value_item_total = float(value_item_total[13:])
+
+assert test_total_price == value_item_total
+print('Test assert total price OK')
+
+tax_var = driver.find_element(By.XPATH, '//*[@id="checkout_summary_container"]/div/div[2]/div[7]')
+value_tax_var = tax_var.text
+value_tax_var = float(value_tax_var[6:])
+
+# Total
+total_var = driver.find_element(By.XPATH, '//*[@id="checkout_summary_container"]/div/div[2]/div[8]')
+value_total_var = total_var.text
+value_total_var = float(value_total_var[8:])
+
+assert test_total_price + value_tax_var == value_total_var
+print('Test Total price OK')
+
+"""Complete"""
+finish_button = driver.find_element(By.XPATH, '//*[@id="finish"]')
+finish_button.click()
+print('Finish button click')
+
+complete_var = driver.find_element(By.XPATH, '//*[@id="checkout_complete_container"]/h2')
+complete_var = complete_var.text
+print(complete_var)
+
+# Screen Complete
+now_date = datetime.datetime.now().strftime("%H.%M.%S-%Y.%m.%d")
+name_screenshot = 'Complete Screenshot' + now_date + '.png'
+driver.save_screenshot(f'screenshots/{name_screenshot}')
+print(name_screenshot)
