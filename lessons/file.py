492629a6a5e58aef5fd479b01473dd20ca761cba
NikolayKuznetsov <kuznetsov.nikolay@lab-company.ru>
2024-08-30 17:28:56 +0500

    Initial commit

--- lessons/file.py
+++ lessons/file.py
@@ -1,12 +1,12 @@
-fw = open('test/test.txt', 'a')
+fw = open('../test/test.txt', 'a')
 fw.write('Hello World\n')
 fw.close()
 
-fw = open('test/test1.txt', 'w')
+fw = open('../test/test1.txt', 'w')
 fw.write('Privet mir!!!')
 fw.close()
 
-fw = open('test/test1.txt', 'r')
+fw = open('../test/test1.txt', 'r')
 text = fw.read()
 fw.close()
 print(text)
